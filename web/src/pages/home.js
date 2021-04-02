b2cef70d1669be5d6eafc174008916c16b3e8213
KalebWithAK <kad9603@gmail.com>
2021-04-02 10:36:21 -0400

    working home page

---
+++ web/src/pages/home.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import MeditationLinechart from '../components/meditationLinechart.js';
+import AddSession from '../components/addSession.js'
+
+class Home extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.refresh = this.refresh.bind(this);
+
+        this.state = {
+            refresh: false
+        }
+    }
+
+    refresh = () => {
+        this.setState({ refreshed: !this.state.refresh });
+    }
+
+    render() {
+        return (
+            <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', /* hotel: trivago */}}>
+                <h1>Meditation</h1>
+                <AddSession refresh={this.refresh} />
+                <MeditationLinechart key={this.state.refreshed} />
+            </div>
+        );
+    }
+}
+
+export default Home;
