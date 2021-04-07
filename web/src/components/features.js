5c3428fa18beb1c15300900f8b58dba480fc9ad2
KalebWithAK <kad9603@gmail.com>
2021-04-07 11:39:06 -0400

    added keys to each feature

--- web/src/components/features.js
+++ web/src/components/features.js
@@ -2,15 +2,16 @@ import React from 'react'
 
 class Features extends React.Component {
     features = [
-        'starting and ending session with buttons',
-        'bell to mark 10, 20, 30, etc minutes'
+        { text: 'starting and ending session with buttons', id: 0 },
+        { text: 'bell to mark 10, 20, 30, etc minutes', id: 1 },
+        { text: 'different types of graphs (bar chart for avg, min, and max times per day, week, month, year', id: 2 }
     ]
     render () {
         return (
             <div>
                 <h1 style={{textAlign: 'center'}}>Features</h1>
                 <ul /*style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}*/>
-                    {this.features.map(feature => <li>{feature}</li>)}
+                    {this.features.map(feature => <li key={feature.id}>{feature.text}</li>)}
                 </ul>
             </div>
         );
